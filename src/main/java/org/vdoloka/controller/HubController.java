d19968cbd76ab4ce7cf42cdd61efe3259984a9ad
Volodymyr Doloka <optimyzator@gmail.com>
2023-02-05 16:47:36 +0200

    hubController Refactor

--- src/main/java/org/vdoloka/controller/HubController.java
+++ src/main/java/org/vdoloka/controller/HubController.java
@@ -1,30 +1,24 @@
 package org.vdoloka.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.vdoloka.entity.Location;
 import org.vdoloka.entity.User;
 import org.vdoloka.repository.impl.LocationsRepositoryImpl;
 import org.vdoloka.service.impl.UsersServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@RequiredArgsConstructor
 @Controller
 public class HubController {
     private final UsersServiceImpl usersService;
     private final LocationsRepositoryImpl locationsRepository;
 
-    @Autowired
-    public HubController(UsersServiceImpl usersService, LocationsRepositoryImpl locationsRepository) {
-        this.usersService = usersService;
-        this.locationsRepository = locationsRepository;
-    }
-
     @GetMapping(path = "/hub/{hubId}")
-    public String viewProfile(@PathVariable(value = "hubId") int hubId, Model model) {
+    public String viewProfile(@PathVariable int hubId, Model model) {
         List<Location> locations = locationsRepository.getLocations();
         model.addAttribute("locations", locations);
         User user = usersService.findByUserID(hubId);

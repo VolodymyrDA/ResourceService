e6c1a11d0b559d758bb9ad512880d352787cb6ba
Volodymyr Doloka <optimyzator@gmail.com>
2023-02-05 16:52:03 +0200

    profileController Refactor

--- src/main/java/org/vdoloka/controller/ProfileController.java
+++ src/main/java/org/vdoloka/controller/ProfileController.java
@@ -1,10 +1,10 @@
 package org.vdoloka.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.vdoloka.entity.Location;
 import org.vdoloka.entity.User;
 import org.vdoloka.repository.impl.LocationsRepositoryImpl;
 import org.vdoloka.service.impl.UsersServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,17 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import javax.validation.Valid;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Controller
 public class ProfileController {
-
     private final UsersServiceImpl usersService;
     private final LocationsRepositoryImpl locationsRepository;
-
-    @Autowired
-    public ProfileController(UsersServiceImpl usersService, LocationsRepositoryImpl locationsRepository) {
-        this.usersService = usersService;
-        this.locationsRepository = locationsRepository;
-    }
 
     @GetMapping("/profile")
     public String viewProfile(Model model) {
@@ -35,16 +29,14 @@ public class ProfileController {
 
     @PostMapping("/profile")
     public String updateProfile(@Valid User user, BindingResult bindingResult, Model model) {
-        model.addAttribute("errors", bindingResult.getFieldErrors());
         if (bindingResult.hasErrors()) {
             model.addAttribute("errors", bindingResult.getFieldErrors());
             List<Location> locations = locationsRepository.getLocations();
             model.addAttribute("locations", locations);
             return "profile";
-        } else {
-            usersService.updateUser(user);
-            model.addAttribute("message", "Update Success");
-            return "login";
         }
+        usersService.updateUser(user);
+        model.addAttribute("message", "Update Success");
+        return "login";
     }
 }

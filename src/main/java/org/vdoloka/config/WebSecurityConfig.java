54506517039819a673dfbc030ffe2cf2c6d960c7
Volodymyr Doloka <optimyzator@gmail.com>
2023-02-19 00:05:11 +0200

    refactor webSecurityConfig

--- src/main/java/org/vdoloka/config/WebSecurityConfig.java
+++ src/main/java/org/vdoloka/config/WebSecurityConfig.java
@@ -1,59 +1,40 @@
 package org.vdoloka.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private String[] ignoreCsrfAntMatchers = {
-            "/dynamic-builder-compress",
-            "/dynamic-builder-general",
-            "/dynamic-builder-specific",
-            "/set-secrets"
-    };
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .csrf().ignoringAntMatchers(ignoreCsrfAntMatchers).and()
                 .authorizeRequests()
-                .antMatchers("/registration", "/css/*").permitAll()
-                .antMatchers("/hubsOrders", "/resources").hasAnyRole("ADMIN", "HUB")
-                .anyRequest().authenticated()
-                .and()
+                    .antMatchers("/registration", "/css/*").permitAll()
+                    .antMatchers("/hubsOrders", "/resources").hasAnyRole("ADMIN", "HUB")
+                    .anyRequest().authenticated()
+                    .and()
                 .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/orders")
-                .permitAll()
-                .and()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/orders")
+                    .permitAll()
+                    .and()
                 .logout()
-                .logoutSuccessUrl("/login")
-                .permitAll()
-                .and()
+                    .logoutSuccessUrl("/login")
+                 .permitAll()
+                    .and()
                 .rememberMe();
+
         return httpSecurity.build();
     }
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setHeaderName("X-XSRF-TOKEN");
-        return repository;
     }
 }

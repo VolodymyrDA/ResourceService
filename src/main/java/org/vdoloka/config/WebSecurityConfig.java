1e8d5c0158d816c568004c2f26b37d87ccab318f
Volodymyr Doloka <optimyzator@gmail.com>
2023-02-18 19:37:37 +0200

    refactor WebSecurityConfig

--- src/main/java/org/vdoloka/config/WebSecurityConfig.java
+++ src/main/java/org/vdoloka/config/WebSecurityConfig.java
@@ -1,30 +1,19 @@
 package org.vdoloka.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.sql.DataSource;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    UserDetailsServiceImpl userDetailsServiceImpl;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/registration", "/css/*").permitAll()
@@ -40,18 +29,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login")
                 .permitAll()
                 .and()
-                .rememberMe()
-                .and()
-        ;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsServiceImpl).
-                and()
-                .jdbcAuthentication()
-                .dataSource(dataSource)
-                .passwordEncoder(encoder());
+                .rememberMe();
+        return httpSecurity.build();
     }
 
     @Bean

a2b6dd2860639423f72c702cf197334041144c0e
Volodymyr Doloka <optimyzator@gmail.com>
2023-02-22 16:25:46 +0200

    refactors users service to optional usage

--- src/main/java/org/vdoloka/repository/UsersRepository.java
+++ src/main/java/org/vdoloka/repository/UsersRepository.java
@@ -2,13 +2,15 @@ package org.vdoloka.repository;
 
 import org.vdoloka.entity.User;
 
+import java.util.Optional;
+
 
 public interface UsersRepository {
     void addUser(User user);
 
     void updateUser(User user);
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
-    User findByUserID(int id);
+    Optional<User> findByUserID(int id);
 }
